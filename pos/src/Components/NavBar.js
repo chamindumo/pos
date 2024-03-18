7271a616e08c4849fd8ab1a4538c8635916851bf
MJAbhayagunawardhana <miyuruabhayagunawardhana@gmail.com>
2024-03-18 21:59:53 +0530

    Change the value

--- pos/src/Components/NavBar.js
+++ pos/src/Components/NavBar.js
@@ -17,7 +17,7 @@ const NavBar = () => {
     const handleclick = () => {
   console.log('Button clicked!');
   const data = [
-    { id: 1, name: 'Alice', age: 30 },
+    { id: 1, name: 'Alice', age: 40},
     { id: 2, name: 'Bob', age: 25 },
     { id: 3, name: 'Charlie', age: 40 },
   ];
