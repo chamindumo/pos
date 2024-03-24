fb9d709b7fde2a1c2fe68f213d0ac4abb09b459f
chamindumo <janithramoramudali@gmail.com>
2024-03-24 16:34:09 +0530

    user page

--- pos/src/App.js
+++ pos/src/App.js
@@ -4,12 +4,8 @@ import Admin from './Components/Admin.js';
 import Login from './Components/Login';
 import ForgotPassword from './Components/Forgot_password';
 import Addnewuser from './Components/Addnewuser.js';
-<<<<<<< HEAD
 import User from './Components/user/user.js';
-=======
 import SignUp from './Components/SignUp.js';
->>>>>>> 235b02b1f7b37b091518ef98b6ad786b72e21e94
-
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 const App = () => {
@@ -21,13 +17,9 @@ const App = () => {
         <Route path="/Admin" element={<Admin />} />
         <Route path="/" element={<Login />} />
         <Route path="/forgot_password" element={<ForgotPassword />} />
-<<<<<<< HEAD
         <Route path="/Addnewuser" element={<Addnewuser/>}/>  
         <Route path="/User" element={<User/>}/>
-=======
-        <Route path="/Addnewuser" element={<SignUp/>}/>  
-
->>>>>>> 235b02b1f7b37b091518ef98b6ad786b72e21e94
+        <Route path="/Addnewuser" element={<SignUp/>}/>
 
       </Routes>
     </Router>
