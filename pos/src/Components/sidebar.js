683eaa9ba7a2219c35db16a3b919f99ee2534a23
UdayanaSanani <udayana.sab@gmail.com>
2024-03-30 00:24:22 +0530

    sidebar update

--- pos/src/Components/sidebar.js
+++ pos/src/Components/sidebar.js
@@ -2,14 +2,15 @@
 import React from "react";
 import { useState } from "react";
 import { Layout } from "antd";
-//import '.F:/React Project/pos/pos/src/Components/Assests/ sidebar.css';
-
+import Logo from "./logo";
 
 const {Header, Sider} = Layout;
 function App() {
 	return (
 	<Layout>
-		<Sider className="sidebar">siderbar</Sider>
+		<Sider className="sidebar">
+			<Logo/>
+		</Sider>
 	</Layout>	
   );
 }
