2b5b2663081a1d100d04633a735d1362836295e2
UdayanaSanani <udayana.sab@gmail.com>
2024-04-17 23:33:02 +0530

    AddButtonImage

--- pos/src/Components/Header.js
+++ pos/src/Components/Header.js
@@ -1,5 +1,12 @@
 import React, { useState } from "react";
 import Profile from '../Components/Assests/profile.png';
+import HomeIcon from '../Components/Assests/home.png';
+import ItemIcon from '../Components/Assests/item.png';
+import PromotionIcon from '../Components/Assests/promotion.png';
+import AddUserIcon from '../Components/Assests/adduser.png';
+import StockIcon from '../Components/Assests/stock.png';
+import BankIcon from '../Components/Assests/bank.png';
+import DiscountIcon from '../Components/Assests/discount.png';
 
 import './Header.css';
 
@@ -20,8 +27,7 @@ const Header = () => {
                     {showProfilePopup && (
                         <div className="profile-popup">
                             {/* Your profile content goes here */}
-                            <p>udayana</p>
-                            <p>user</p>
+                            <p>Udayana Sajani</p>
                             <button className='sidebar-button' onClick=''>LOG OUT</button>
                         </div>
                     )}
@@ -30,13 +36,34 @@ const Header = () => {
 
             </div>
             <div className="sidebar">
-                <button className='sidebar-button' onClick=''>DASHBOARD</button>
-                <button className='sidebar-button' onClick=''>ITEMS</button>
-                <button className='sidebar-button' onClick=''>PROMOTON</button>
-                <button className='sidebar-button' onClick=''>ADD USER</button>
-                <button className='sidebar-button' onClick=''>STOCK</button>
-                <button className='sidebar-button' onClick=''>BANKS</button>
-                <button className='sidebar-button' onClick=''>DISCOUNT</button>
+                <button className='sidebar-button' onClick=''>
+                    <img src={HomeIcon} alt="Home" className="sidebar-icon" />
+                        DASHBOARD
+                </button>
+                <button className='sidebar-button' onClick=''>
+                    <img src={ItemIcon} alt="Item" className="sidebar-icon" />
+                        ALLITEMS
+                </button>
+                <button className='sidebar-button' onClick=''>
+                    <img src={PromotionIcon} alt="Item" className="sidebar-icon" />
+                        PROMOTON
+                </button>
+                <button className='sidebar-button' onClick=''>
+                    <img src={AddUserIcon} alt="Item" className="sidebar-icon" />
+                        ADDUSER
+                </button>
+                <button className='sidebar-button' onClick=''>
+                    <img src={StockIcon} alt="Item" className="sidebar-icon" />
+                        STOCK
+                </button>
+                <button className='sidebar-button' onClick=''>
+                    <img src={BankIcon} alt="Item" className="sidebar-icon" />
+                        BANKS
+                </button>
+                <button className='sidebar-button' onClick=''>
+                    <img src={DiscountIcon} alt="Item" className="sidebar-icon" />
+                        DISCOUNT
+                </button>
             </div>
         </nav>
 
